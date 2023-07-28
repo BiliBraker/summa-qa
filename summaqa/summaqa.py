bb35e4c2102cafc6393481318bc8e5f6199908bd
Krisztián Boros <soirkorob@gmail.com>
2023-07-28 10:22:55 +0900

    fix tqdm

--- summaqa/summaqa.py
+++ summaqa/summaqa.py
@@ -94,7 +94,7 @@ def evaluate_corpus(srcs, gens, model=None, questionss=None, aswss=None):
 
     global_score = {"avg_prob": 0, "avg_fscore": 0}
 
-    for i, (src, gen) in tqdm.tqdm(enumerate(zip(srcs, gens))):
+    for i, (src, gen) in enumerate(zip(tqdm.tqdm(srcs), gens)):
 
         # if questionss is None, generate the questions and answers else get the corrisponding ones.
         if not questionss:
